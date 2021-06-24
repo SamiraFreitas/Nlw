ad52cbcfac189499e63e30f93ead80e65ce14bf5
SamiraFreitas <73719899+SamiraFreitas@users.noreply.github.com>
2021-06-24 14:51:11 -0300

    aula3_inicio

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,9 +1,9 @@
 //componente e uma função que devolve html
-import { BrowserRouter, Route} from 'react-router-dom'
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import {Home} from './pages/Home'
 import { NewRoom } from "./pages/NewRoom";
-
+import {Room} from "./pages/Room"
 import { AuthContextProvider } from './context/AuthContext'
 
 function App() {
@@ -11,8 +11,11 @@ function App() {
   return (
     <BrowserRouter>
    <AuthContextProvider>
+     <Switch>
     <Route  path="/" exact  component={Home}/>
-    <Route  path="/rooms/new" component={NewRoom}/>
+    <Route  path="/rooms/new"   component={NewRoom}/>
+    <Route  path="/rooms/:id" component={Room}/>
+    </Switch>
     </AuthContextProvider>
     </BrowserRouter>
   );
